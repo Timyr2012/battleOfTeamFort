a057ae81d897f0157ceab7cb962e9ca5a39b2457
rotmarer23 <167620937+Timyr2012@users.noreply.github.com>
2026-03-10 17:35:20 +0200

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+
+        Random rand = new Random();
+        static void Main(string[] args)
+        {
+            var rand = new Random();
+            Console.WriteLine("Введіть своє ім'я чаклуне");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Введіть свій рівень магії {name}");
+            double forceOfMagic = int.Parse(Console.ReadLine());
+            if (forceOfMagic < 1)
+            {
+                forceOfMagic = 1;
+            }
+
+            else if (forceOfMagic > 10)
+            {
+                forceOfMagic = 10;
+            }
+            double forceOfCristal = rand.Next(1, 100);
+            double rampedUpPower = Math.Pow(forceOfMagic, 2);
+            double finalspellpower = Math.Round(rampedUpPower + forceOfCristal / 2);
+            Console.WriteLine($"Вітаю {name}!");
+            Console.WriteLine($"Твій рівень магії {forceOfMagic}");
+            Console.WriteLine($"Сила кристала твого {forceOfCristal}");
+            Console.WriteLine($"Фінальний посилений магічний удар має силу в {finalspellpower} одиниць шкоди");
+
+
+        }
+    }
+}
